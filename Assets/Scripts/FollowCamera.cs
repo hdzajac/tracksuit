558a5fd1360cdb1e54af4edb4da5805305940c71
Hubert Zając <hubert.zajac@outlook.com>
2019-03-18 17:02:59 +0100

    tracksuit 3rd person with position

--- Assets/Scripts/FollowCamera.cs
+++ Assets/Scripts/FollowCamera.cs
@@ -8,12 +8,17 @@ using InputTracking = UnityEngine.XR.InputTracking;
 
 public class FollowCamera : MonoBehaviour
 {
+    public  GameObject target;
     void Start() 
     {
         InputTracking.disablePositionalTracking = false;
         var rotation = Utils.getYAxisRotation(transform.rotation);
-        transform.parent.parent.localRotation = Quaternion.Inverse(rotation);
-
+//        target.transform.localRotation = Quaternion.Inverse(rotation);
+        var transform1 = transform;
+        var newPosition = transform1.position + transform1.forward * 2;
+        newPosition.y = 0;
+        target.transform.position = newPosition;
+        target.transform.rotation = rotation;
     }
 
 }
